5cd7ffc30e19bf0a21166ee3a405384d6df6e5bb
def-dani22 <esteban.gomez.rivera@cuc.cr>
2024-06-12 00:14:59 -0600

    Primer commit vistas y base de datos

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,130 @@
+﻿using MotoRiders.CR.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+
+namespace MotoRiders.CR.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+
+        private string connectionString = "Data Source=DESKTOP-KNSONQV\\PUBLICADOR;Initial Catalog=motoriders;Integrated Security=True;";
+
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+            List<Producto> motos = new List<Producto>();
+
+            // Consulta SQL para recuperar datos de la tabla Motos
+            string query = "SELECT * FROM Productos WHERE tipo = 'MOTO'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Producto moto = new Producto();
+                            moto.Id = Convert.ToInt32(reader["id"]);
+                            moto.Img = reader["img"].ToString();
+                            moto.Tipo = reader["tipo"].ToString();
+                            moto.Nombre = reader["nombre"].ToString();
+                            moto.Modelo = reader["modelo"].ToString();
+                            moto.Color = reader["color"].ToString();
+                            moto.Descripcion = reader["descripcion"].ToString();
+                            moto.Caracteristicas = reader["caracteristicas"].ToString();
+                            moto.PrecioVenta = Convert.ToDecimal(reader["precioVenta"]);
+                            moto.PrecioAlquiler = Convert.ToDecimal(reader["precioAlquiler"]);
+                            motos.Add(moto);
+                        }
+                    }
+                }
+            }
+
+            // Pasar los datos a la vista
+            return View(motos);
+
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MotoRiders.CR.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string Img { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Tipo { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Color { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Caracteristicas { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal PrecioVenta { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? PrecioAlquiler { get; set; }
+    }
+}
