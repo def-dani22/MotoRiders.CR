253c72de16d5dff38f9be0361b2bdb6acef89b0e
def-dani22 <esteban.gomez.rivera@cuc.cr>
2024-07-15 23:26:00 -0600

    Funcional hasta vistas de reportes, logout...

---
+++ Controllers/AuditoriaController.cs
@@ -0,0 +1,76 @@
+﻿//using MotoRiders.CR.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+
+//namespace MotoRiders.CR.Controllers
+//{
+//    public class AuditoriaController : Controller
+//    {
+//        // GET: Auditoria
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
+
+
+
+
+
+using MotoRiders.CR.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MotoRiders.CR.Controllers
+{
+
+    public class AuditoriaController : Controller
+    {
+        private string connectionString = "Data Source=DESKTOP-KNSONQV\\PUBLICADOR;Initial Catalog=motoriders;Integrated Security=True;";
+
+        // GET: Repuestos
+        public ActionResult Index()
+        {
+            List<Auditoria> auditorias = new List<Auditoria>();
+
+            // Consulta SQL para recuperar datos de la tabla Talleres
+            string query = "SELECT * FROM Auditoria"; // Puedes ajustar la consulta según tus necesidades
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Auditoria auditoria = new Auditoria();
+                            auditoria.Id = Convert.ToInt32(reader["id"]);
+                            auditoria.Usuario = reader["usuario"].ToString();
+                            auditoria.Accion = reader["accion"].ToString();
+                            auditoria.Detalles = reader["detalles"].ToString();
+                            auditoria.FechaHora = Convert.ToDateTime(reader["fechahora"]);
+                            auditoria.IPAddress = reader["ipaddress"].ToString();
+                            auditorias.Add(auditoria);
+
+
+                        }
+                    }
+                }
+            }
+
+            // Pasar los datos a la vista
+            return View(auditorias);
+        }
+    }
+}
--- Controllers/HomeAnalistaDatosController.cs
+++ Controllers/HomeAnalistaDatosController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MotoRiders.CR.Controllers
 {
@@ -12,6 +14,62 @@ namespace MotoRiders.CR.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+
+
+        public static class AuditoriaHelper
+        {
+            private static string connectionString = "Data Source=DESKTOP-KNSONQV\\PUBLICADOR;Initial Catalog=motoriders;Integrated Security=True;";
+
+            public static void RegistrarAccion(string usuario, string accion, string detalles = null, string ipAddress = null)
+            {
+                string query = @"
+            INSERT INTO Auditoria (Usuario, Accion, Detalles, FechaHora, IPAddress) 
+            VALUES (@Usuario, @Accion, @Detalles, @FechaHora, @IPAddress)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Usuario", usuario);
+                        command.Parameters.AddWithValue("@Accion", accion);
+                        command.Parameters.AddWithValue("@Detalles", detalles ?? (object)DBNull.Value);
+                        command.Parameters.AddWithValue("@FechaHora", DateTime.Now);
+                        command.Parameters.AddWithValue("@IPAddress", ipAddress ?? (object)DBNull.Value);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+
+
+        [HttpPost]
+        public ActionResult CerrarSesion()
+        {
+            try
+            {
+                // Obtener el email del usuario autenticado
+                string email = User.Identity.Name;
+                string ipAddress = Request.UserHostAddress; // Obtener la IP del usuario
+
+                // Registrar acción de auditoría para el cierre de sesión
+                AuditoriaHelper.RegistrarAccion(email, "Cierre de Sesión", "El usuario cerró sesión.", ipAddress);
+
+                // Cerrar sesión
+                FormsAuthentication.SignOut();
+            }
+            catch (Exception ex)
+            {
+                // Manejar la excepción adecuadamente (log, mensaje al usuario, etc.)
+                throw new Exception("Error al cerrar sesión.", ex);
+            }
+
+            return RedirectToAction("InicioSesion", "Cuenta");
         }
     }
 }
