6749e00cd694c083520068e2f6b0b9fa719c89a8
def-dani22 <esteban.gomez.rivera@cuc.cr>
2024-08-05 21:53:19 -0600

    FUNCIONAL APIS -REGISTRARCLIENTE

---
+++ Models/PersonaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotoRiders.CR.Models
+{
+    public class PersonaModel
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
