dd7c13c07d482d225b28e52cf0e2e4c01166ff17
def-dani22 <esteban.gomez.rivera@cuc.cr>
2024-08-20 00:39:57 -0600

    Ajustes PWA

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,97 @@
-﻿const CACHE_NAME = 'motoRiders-cache-v1';
-const URLsToCache = [
-    '/',                        // Página principal (index.html)
-    '/Content/Styles.css',       // Hoja de estilo
-    '/Content/Script.js',        // Script de JavaScript
-    '/Content/Imagenes/ima-192x192.png',  // Icono 192x192
-    '/Content/Imagenes/ima-512x512.png'   // Icono 512x512
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
+﻿//asignar un nombre y versión al cache
+const CACHE_NAME = 'xamplepwa',
+    urlsToCache = [
+        '/',
+        '/Views/Cuenta/InicioSesion.cshtml',
+        '/Content/Script.js',
+        '/Content/Site.css',
+        '/Content/Styles.css',
+        '/Views/Auditoria/index.cshtml',
+        '/Views/Carrito/index.cshtml',
+        '/Views/CarritoCompra/index.cshtml',
+        '/Views/Contacto/index.cshtml',
+        '/Views/Cotizacion/Create.cshtml',
+        '/Views/Cotizacion/index.cshtml',
+        '/Views/CotizacionPersonalizada/index.cshtml',
+        '/Views/Cuadraciclo/index.cshtml',
+        '/Views/Cuenta/Confirmacion.cshtml',
+        '/Views/Cuenta/MiPerfil.cshtml',
+        '/Views/Cuenta/OlvideMiContrasena.cshtml',
+        '/Views/Cuenta/RecuperarContrasena.cshtml',
+        '/Views/Cuenta/Registro.cshtml',
+        '/Views/Cuenta/ResponderPreguntaSeguridad.cshtml',
+        '/Views/Cuenta/VerificarCorreo.cshtml',
+        '/Views/Cuenta/VerificarToken.cshtml',
+        '/Views/Cuenta/VerificarToken2FA.cshtml',
+        '/Views/Home/index.cshtml',
+        '/Views/HomeAnalistaDatos/index.cshtml',
+        '/Views/HomeAnalistaDatos/MiPerfil.cshtml',
+        '/Views/IndicadoresEconomicosBCCR/index.cshtml',
+        '/Views/MotosElectricas/index.cshtml',
+        '/Views/Producto/index.cshtml',
+        '/Views/Prueba/Confirmacion.cshtml',
+        '/Views/Prueba/Nueva.cshtml',
+        '/Views/PruebaManejo/Confirmacion.cshtml',
+        '/Views/PruebaManejo/index.cshtml',
+        '/Views/Reporte/AccesoDenegado.cshtml',
+        '/Views/Reporte/index.cshtml',
+        '/Views/ReporteCompras/index.cshtml',
+        '/Views/ReporteCotización/index.cshtml',
+        '/Views/ReportePruebaManejo/index.cshtml',
+        '/Views/Repuestos/index.cshtml',
+        '/Views/Shared/_Layout.cshtml',
+        '/Views/Shared/_Onboarding.cshtml',
+        '/Views/Talleres/index.cshtml',
+        '/Views/_ViewStart.cshtml',
+        '/Content/Imagenes/Ob2.PNG',
+        '/Content/Imagenes/Ob3.PNG',
+        '/Content/Imagenes/Onboarding1.png',
+        '/Content/Imagenes/c1.png',
+        '/Content/Imagenes/ima-512x512.png',
+        '/Content/Imagenes/ima-72x72.png',
+    ]
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+    e.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(URLsToCache);
+            .then(cache => {
+                return cache.addAll(urlsToCache)
+                    .then(() => self.skipWaiting())
             })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
+            .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+    e.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        //Eliminamos lo que ya no se necesita en cache
+                        if (cacheWhitelist.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName)
+                        }
+                    })
+                )
             })
-    );
-});
-
-self.addEventListener('sync', (event) => {
-    if (event.tag === 'sync-data') {
-        event.waitUntil(
-            fetch('/api/sync-data')
-                .then((response) => response.json())
-                .then((data) => {
-                    // Procesar los datos sincronizados
-                })
-        );
-    }
-});
+            // Le indica al SW activar el cache actual
+            .then(() => self.clients.claim())
+    )
+})
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => {
+                if (res) {
+                    //recuperar del cache
+                    return res
+                }
+                //recuperar de la petición a la url
+                return fetch(e.request)
+            })
+    )
+})
